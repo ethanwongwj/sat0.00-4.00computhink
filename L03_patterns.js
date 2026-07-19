f6cce6506a5226bd0d9f85f0f65f2c7f9a14b6d9
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-19 04:31:11 +0000

    Jul 19, 2026, 4:31 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -13,27 +13,27 @@ function draw() {
     // circle(350,50,50)
 
 
-let circleDiameter = 50;
-let circleY = 50;
-for (let i = 1; i <10;i++ ) {
-  let circol = ( i * 50 )
-  if (circol > 254){
-    circol = circol - circol
-  }
-  fill(circol,0,0)
-  circle(50*i, circleY*i, circleDiameter)
-  // circle(50*i, circleY, circleDiameter)
-  // circle(50, circleY*i, circleDiameter)
-  print("i will not sling mud at my friend")
-  print(width)
-  print(height)
-}
+// let circleDiameter = 50;
+// let circleY = 50;
+// for (let i = 1; i <10;i++ ) {
+//   let circol = ( i * 50 )
+//   if (circol > 254){
+//     circol = circol - circol
+//   }
+//   fill(circol,0,0)
+//   circle(50*i, circleY*i, circleDiameter)
+//   // circle(50*i, circleY, circleDiameter)
+//   // circle(50, circleY*i, circleDiameter)
+//   print("i will not sling mud at my friend")
+//   print(width)
+//   print(height)
+// }
   // Recap 1: Repeating Circles\
 
   // Task 1: Colour Gradient
 
   // Task 2: Colour Loop
-
+  circle()
   // Task 3: Row of Circles
 
   // Task 4: Grid of Circles
