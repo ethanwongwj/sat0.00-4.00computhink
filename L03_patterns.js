62a86fc410b20f2dbb84b2c5a660a0dc4af86de4
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-19 04:23:37 +0000

    Jul 19, 2026, 4:23 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -23,7 +23,7 @@ for (let i = 1; i <7;i++ ) {
   circle(50*i, circleY*i, circleDiameter)
   // circle(50*i, circleY, circleDiameter)
   // circle(50, circleY*i, circleDiameter)
-  
+  print("i will not sling mud at my friend")
   print(width)
   print(height)
 }
