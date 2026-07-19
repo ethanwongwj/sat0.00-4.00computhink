b7e078676bab3dd7251d76d03b141d5fca750d9e
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-19 03:28:36 +0000

    Jul 19, 2026, 3:28 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -57,6 +57,30 @@ function draw() {
   circle(258,250,20)
   fill("green")
   rect(233,300,50,25)
+  fill("orange")
+  ellipse(58,100,55,120)
+  fill("gold");
+  triangle(30, 75, 58, 20, 86, 75);
+  fill("gray")
+  circle(58,100,20)
+  fill("green")
+  rect(33,150,50,25)
+  fill("orange")
+  ellipse(158,100,55,120)
+  fill("gold");
+  triangle(130, 75, 158, 20, 186, 75);
+  fill("gray")
+  circle(158,100,20)
+  fill("green")
+  rect(133,150,50,25)
+  fill("orange")
+  ellipse(258,100,55,120)
+  fill("gold");
+  triangle(230, 75, 258, 20, 286, 75);
+  fill("gray")
+  circle(258,100,20)
+  fill("green")
+  rect(233,150,50,25)
 }
 
 
