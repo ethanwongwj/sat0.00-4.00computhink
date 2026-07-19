5587103837fbc20e1f026ab6fcf65aa362529ff9
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-19 04:10:06 +0000

    Jul 19, 2026, 4:10 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -15,7 +15,7 @@ function draw() {
 let circleDiameter = 50;
 let circleY = 50;
 for (let i = 1; i < 11; i++ ) {
-  fill(i*25)
+  fill(i * 25)
   circle(50*i, circleY*i, circleDiameter)
   // circle(50*i, circleY, circleDiameter)
   // circle(50, circleY*i, circleDiameter)
