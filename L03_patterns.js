5053c35d64568cd945da48b6fc9f215bdebdd8e9
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-19 04:33:49 +0000

    Jul 19, 2026, 4:33 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -13,17 +13,17 @@ function draw() {
     // circle(350,50,50)
 
 
-// let circleDiameter = 50;
-// let circleY = 50;
-// for (let i = 1; i <10;i++ ) {
-//   let circol = ( i * 50 )
-//   if (circol > 254){
-//     circol = circol - circol
-//   }
-//   fill(circol,0,0)
-//   circle(50*i, circleY*i, circleDiameter)
-//   // circle(50*i, circleY, circleDiameter)
-//   // circle(50, circleY*i, circleDiameter)
+ let circleDiameter = 50;
+ let circleY = 50;
+ for (let i = 1; i <10;i++ ) {
+   let circol = ( i * 50 )
+   if (circol > 254){
+     circol = circol - circol
+   }
+   fill(circol,0,0)
+   circle(50*i, circleY*i, circleDiameter)
+   // circle(50*i, circleY, circleDiameter)
+   // circle(50, circleY*i, circleDiameter)
 //   print("i will not sling mud at my friend")
 //   print(width)
 //   print(height)
