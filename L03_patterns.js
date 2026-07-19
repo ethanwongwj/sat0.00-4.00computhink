d01c572cce4dc3cee4c23aa42e41dcf1ba360dae
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-19 03:53:35 +0000

    Jul 19, 2026, 3:53 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -12,7 +12,9 @@ function draw() {
     circle(350,50,50)
 }
 
-for (let i = 1; i < 11; i++ ) {}
+for (let i = 1; i < 11; i++ ) {
+  
+}
   // Recap 1: Repeating Circles\
 
   // Task 1: Colour Gradient
