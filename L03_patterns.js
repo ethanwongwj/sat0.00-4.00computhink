1fb3cd45be9bb6f408ac08dde731856f2216c03d
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-25 06:36:24 +0000

    Jul 25, 2026, 6:36 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -27,8 +27,9 @@ let Colour = 0;
 
 let count = 0
 for (let y = 1; y < 10 ; y++) {
-  fill(count % 2 * 255)
+  fill((count % 2) * 255)
   circle(50*y , 50*2, 50)
+  count++
 }
    print("i will not say 67")
    print(width)
