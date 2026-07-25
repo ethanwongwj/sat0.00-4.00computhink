8122b71c05a86c1b881b9dac54c08c95126c8b39
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-25 07:04:20 +0000

    Jul 25, 2026, 7:04 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -31,7 +31,7 @@ function setup() {
 //    print(height)
 //  }
 function draw() {
-  for (let row = 0; row <5; row++) {
+  for (let row = 0; row <10; row++) {
     for (let col = 0; col < 5; col++) { 
       if ((row + col) % 2 === 0) {
         fill(0);
