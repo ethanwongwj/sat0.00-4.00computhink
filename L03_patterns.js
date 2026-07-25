b145bccc8f2ed524fb4eeecbd83abb3f2c1d07c7
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-25 06:35:55 +0000

    Jul 25, 2026, 6:35 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -5,26 +5,31 @@ function setup() {
 
 let Colour = 0;
   
-for ( let t = 1; t <8;t++) {
-   function draw() {
-     fill("blue")
-     // circle(50,50,50)
-     // circle(150,50,50)
-     // circle(250,50,50)
-     // circle(350,50,50)
 
+  function draw() {
+    fill("blue")
+    // circle(50,50,50)
+    // circle(150,50,50)
+    // circle(250,50,50)
+    // circle(350,50,50)
 
-  let circleDiameter = 50;
-  let circleY = 50;
-  for (let i = 1; i <10;i++ ) {
-    fill(Colour);
-   Colour = Colour + 0.5;
-   if (Colour > 255) {
-     Colour = 0
-   }
-    circle(50*i, circleY*t, circleDiameter)
-    // circle(50*i, circleY, circleDiameter)
-    // circle(50, circleY*i, circleDiameter)
+ let circleDiameter = 50;
+ let circleY = 50;
+ for (let i = 1; i <10;i++ ) {
+   fill(Colour);
+  Colour = Colour + 0.5;
+  if (Colour > 255) {
+    Colour = 0
+  }
+   circle(50*i, circleY ,  circleDiameter)
+   // circle(50*i, circleY, circleDiameter)
+   // circle(50, circleY*i, circleDiameter)
+
+let count = 0
+
+for (let y = 1; y < 10 ; y++) {
+  fill(count % 2 * 255)
+  circle(50*y , 50*2, 50)
 }
    print("i will not say 67")
    print(width)
