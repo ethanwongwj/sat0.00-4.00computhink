f48ba0a62815dba54417ec3e6d655e9cdb0921a5
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-25 06:41:59 +0000

    Jul 25, 2026, 6:41 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -5,13 +5,13 @@ function setup() {
 
 let Colour = 0;
   
-
-  function draw() {
+function draw() {
     fill("blue")
     // circle(50,50,50)
     // circle(150,50,50)
     // circle(250,50,50)
     // circle(350,50,50)
+
 
  let circleDiameter = 50;
  let circleY = 50;
@@ -19,17 +19,13 @@ let Colour = 0;
    fill(Colour);
   Colour = Colour + 0.5;
   if (Colour > 255) {
-    Colour = 0
+    for (let t = 1; t <511; t++){
+      colour = colour
+    }
   }
-   circle(50*i, circleY ,  circleDiameter)
+   circle(50*i, circleY*i, circleDiameter)
    // circle(50*i, circleY, circleDiameter)
    // circle(50, circleY*i, circleDiameter)
-
-let count = 0
-for (let y = 1; y < 10 ; y++) {
-  fill((count % 2) * 255)
-  circle(50*y , 50*2, 50)
-}
    print("i will not say 67")
    print(width)
    print(height)
