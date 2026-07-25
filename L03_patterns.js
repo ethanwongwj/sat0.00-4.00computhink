ab676c7eaeca0f5d99352cf46f81a5563b8b60b8
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-25 06:13:50 +0000

    Jul 25, 2026, 6:13 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -20,7 +20,7 @@ function draw() {
   Colour = Colour + 0.5;
   if (Colour > 255) {
     for (let t = 1; t <511; t++){
-      
+      colour = colour - 0.5
     }
   }
    circle(50*i, circleY*i, circleDiameter)
