4515df23ade37c8cddb44c58e28568a3a782d297
Ethan Wong <wongwj.ethan@gmail.com>
2026-07-25 06:46:35 +0000

    Jul 25, 2026, 6:46 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -5,31 +5,32 @@ function setup() {
 
 let Colour = 0;
   
-function draw() {
-    fill("blue")
-    // circle(50,50,50)
-    // circle(150,50,50)
-    // circle(250,50,50)
-    // circle(350,50,50)
+// function draw() {
+//     fill("blue")
+//     // circle(50,50,50)
+//     // circle(150,50,50)
+//     // circle(250,50,50)
+//     // circle(350,50,50)
 
 
- let circleDiameter = 50;
- let circleY = 50;
- for (let i = 1; i <10;i++ ) {
-   fill(Colour);
-  Colour = Colour + 0.5;
-  if (Colour > 255) {
-    for (let t = 1; t <511; t++){
-      colour = colour + 1
-    }
-  }
-   circle(50*i, circleY, circleDiameter)
-   // circle(50*i, circleY, circleDiameter)
-   // circle(50, circleY*i, circleDiameter)
-   print("i will not say 67")
-   print(width)
-   print(height)
- }
+//  let circleDiameter = 50;
+//  let circleY = 50;
+//  for (let i = 1; i <10;i++ ) {
+//    fill(Colour);
+//   Colour = Colour + 0.5;
+//   if (Colour > 255) {
+//     for (let t = 1; t <511; t++){
+//       colour = colour + 1
+//     }
+//   }
+//    circle(50*i, circleY, circleDiameter)
+//    // circle(50*i, circleY, circleDiameter)
+//    // circle(50, circleY*i, circleDiameter)
+//    print("i will not say 67")
+//    print(width)
+//    print(height)
+//  }
+for (let x = 0; x < width; x += 50) {
   // Recap 1: Repeating Circles\
 
   // Task 1: Colour Gradient
