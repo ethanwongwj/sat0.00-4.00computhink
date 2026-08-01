f8a28c89c17132597e36c862ae844f979d124a9d
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-01 07:30:26 +0000

    Aug 1, 2026, 7:30 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -38,7 +38,7 @@
 //     ellipse(200,200,size)
 // }
 function mouseDragged() {
-    for(i = 0; 1 < 10; i ++)
+    for(i = 0; 1 < 5; i ++)
         fill(random(255),random(255),random(255))
         circle(mouseX, mouseY , random(25))
 }
