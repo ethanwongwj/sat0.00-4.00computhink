ae8bb73ddc68cd7a30fa729c9687164e1433f6a5
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-08 06:43:34 +0000

    Aug 8, 2026, 6:43 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -14,7 +14,7 @@ function setup() {
 // }
 function keyPressed() {
     let numcir = random(5,20)
-    for(i = 0 ; 1 < numcir ; 1++ ) {
+    for(i = 0 ; i < numcir ; i++ ) {
         let colour = colour(random(0,255), random(0,255),random(0,255)) 
         let cirsize = random(10,50);
         let x = random(0,400);
