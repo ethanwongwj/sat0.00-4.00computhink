e8fbd543f795e2063329021e0ed2f311094410fe
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-08 06:32:29 +0000

    Aug 8, 2026, 6:32 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -2,7 +2,7 @@ let rectsize = 50 ;
 function setup() {
     createCanvas(400,400)
 }
- function draw() {
+// function draw() {
 //     background(220)
 //     rect(width / 2 - rectsize / 2, height /2 - rectsize / 2, rectsize,rectsize);
 // }
@@ -26,6 +26,6 @@ function keyPressed() {
     }
 }
 function keyReleased() {
-    
+
 }
 }
