9d094c53c3085c21e1b67ac570bb1dd885a50abf
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-15 06:32:43 +0000

    Aug 15, 2026, 6:32 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -27,12 +27,13 @@ function setup() {
 // function keyReleased() {
 //     createCanvas(400,400)
 // }
+col = 0
 function keyPressed() {
     console.log("key:", key)
     console.log("keycode:", keyCode)
     if (keyCode === 67) {
         background(220)
-        fill("white")
+        fill(col)
         ellipse(200,200,100,100)
     }
 
