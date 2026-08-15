83a9cb81f3ada9500b0c7cf54d018e284861d9c1
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-15 07:04:01 +0000

    Aug 15, 2026, 7:04 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -80,12 +80,14 @@ let x = 0
 function draw() {
     background(220);
     circle(width /2 + x, height/2, 100)
+
     if (keyIsDown(RIGHT_ARROW)) {
         x += 2
     }
     if (keyIsDown(LEFT_ARROW)) {
         x -= 2
     }
+    
     //circle(width /2 + x, height/2, 100)
     if (x < 50) {
         x = 50;
