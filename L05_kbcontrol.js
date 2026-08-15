2a077d0fcd3e98f254695bf30e3951b588717385
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-15 06:40:38 +0000

    Aug 15, 2026, 6:40 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -61,12 +61,14 @@ function mouseMoved() {
         r = 0
         g = 0
         b = 255
-        col = r , g , b
         fill(col)
         ellipse(200,200,100,100)
     }
     if (mouseX < 325 && mouseX > 250 ) {
-        col = 255 , 255 , 0
+        r = 255
+        g = 255
+        b = 0
+        col = r , g , b
         fill(col)
         ellipse(200,200,100,100)
     }
