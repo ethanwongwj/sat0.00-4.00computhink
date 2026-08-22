ebd56acf61145b86bb7ed656250e3a810e3a6d95
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-22 06:56:58 +0000

    Aug 22, 2026, 6:56 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -37,5 +37,5 @@ let soundEffect, bgMusic, statisImage
 function preload() {
     soundEffect = loadSound('assets/pop.mp3');
     bgMusic = loadSound('assets/bossaNova.mp3');
-    staticImage = loadImage('assets/pico-a.png')
+    staticImage = loadImage('assets/pico-a.png');
 }
