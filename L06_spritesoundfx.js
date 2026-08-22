238c9dd6914a28c0e0aabab09d93aa8bbae280a7
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-22 07:30:21 +0000

    Aug 22, 2026, 7:30 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -62,8 +62,8 @@ function draw() {
     if(keyIsDown(LEFT_ARROW)){
         x -= speed;
     }
-    if(x === 400 || x === 0) {
-        
+    if(x ===  || x === 0) {
+
     }
     x = constrain(x,0,width - 100);
     y = constrain(y,0,height - 100);
