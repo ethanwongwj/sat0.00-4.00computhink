e9868d40bb8a04ccba655e8cc145069215c3d347
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-22 06:53:52 +0000

    Aug 22, 2026, 6:53 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,31 +1,34 @@
+// let xPos = 175
+// let yPos = 175
+// let col = 200
 
-function setup() {
-    createCanvas(400,400);
-    background(220);
-}
+// function setup() {
+//     createCanvas(400,400);
+//     background(220);
+// }
 
-function draw() {
-    background(220);
+// function draw() {
+//     background(220);
     
-    fill(col);
-    noStroke();
+//     fill(col);
+//     noStroke();
 
-    if(keyIsDown(UP_ARROW)) {
-       yPos -= 3;
-    }
+//     if(keyIsDown(UP_ARROW)) {
+//        yPos -= 3;
+//     }
         
-    if(keyIsDown(DOWN_ARROW)){
-        yPos += 3;
-    }
-    if(keyIsDown(LEFT_ARROW)) {
-        xPos -= 3;
-    }
-    if(keyIsDown(RIGHT_ARROW)){
-        xPos += 3;
-    }
+//     if(keyIsDown(DOWN_ARROW)){
+//         yPos += 3;
+//     }
+//     if(keyIsDown(LEFT_ARROW)) {
+//         xPos -= 3;
+//     }
+//     if(keyIsDown(RIGHT_ARROW)){
+//         xPos += 3;
+//     }
 
-    xPos = constrain(xPos, 50, 300);
-    yPos = constrain(yPos, 50, 300);
+//     xPos = constrain(xPos, 50, 300);
+//     yPos = constrain(yPos, 50, 300);
 
-    rect( xPos , yPos , 50 , 50 );
-}
+//     rect( xPos , yPos , 50 , 50 );
+// }
