9ead2a621df91e94387b8545c5bca0e3fa6654c3
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-22 06:27:27 +0000

    Aug 22, 2026, 6:27 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -6,8 +6,11 @@ let Xpos = 150
 let Ypos = 150
 function draw() {
     rect( Xpos , Ypos , 50 , 50 );
+    (noStroke)
 }
 function keypressed() {
     if(key === "UP_ARROW")
-        Xpos = Xpos + 1
+        Ypos = Ypos + 3
+    if(key === "DOWN_ARROW")
+        Ypos = 
 }
