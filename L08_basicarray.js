7220bd95ca02b5ca865cb1dfa77a5ebf3ac3f095
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-30 03:43:49 +0000

    Aug 30, 2026, 3:43 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -9,7 +9,7 @@ function setup() {
 function draw() {
     background(220);
     textSize(18);
-    storyText[0] = "A LOOOOOOOOOOOOOONG TIME AGO.........."
+    storyText[0] = "A LOOOOOOOOOOOOOONG TIME AGO..........";
     storytext[1] = "MY GRANDMA TOLD ME A STORY";
     storyText[2] = "AND THE END";
     for(let count = 0 ; count < storyText.length; count++ ) {
