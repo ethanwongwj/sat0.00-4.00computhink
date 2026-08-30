e60e2f943f735693082412fa0037eb3eaca95e60
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-30 03:44:04 +0000

    Aug 30, 2026, 3:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -4,14 +4,15 @@ let lineY = height
 function setup() {
     createCanvas(400,600);
     background(220);
+      storyText[0] = "A LOOOOOOOOOOOOOONG TIME AGO..........";
+    storytext[1] = "MY GRANDMA TOLD ME A STORY";
+    storyText[2] = "AND THE END";
+    
 }
 
 function draw() {
     background(220);
     textSize(18);
-    storyText[0] = "A LOOOOOOOOOOOOOONG TIME AGO..........";
-    storytext[1] = "MY GRANDMA TOLD ME A STORY";
-    storyText[2] = "AND THE END";
     for(let count = 0 ; count < storyText.length; count++ ) {
         text(storyText(count), 100, ypos + liney);
         lineY = lineY + 20;
