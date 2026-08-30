eb5079ca2cc9397991ffd6923d8b520b05d8941c
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-30 03:46:20 +0000

    Aug 30, 2026, 3:46 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -14,7 +14,7 @@ function draw() {
     background(220);
     textSize(18);
     for(let count = 0 ; count < storyText.length; count++ ) {
-        text(storyText(count), 100, ypos + liney);
+        text(storyText[count], 100, ypos + liney);
         textY = lineY + 20;
     }
     textY -= 0.67;
