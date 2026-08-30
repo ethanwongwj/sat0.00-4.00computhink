bdb8651c25cef1c880d88353f12ad842bafbcd9c
Ethan Wong <wongwj.ethan@gmail.com>
2026-08-30 03:32:40 +0000

    Aug 30, 2026, 3:32 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -7,7 +7,7 @@ function setup() {
 function draw() {
     background(220);
     textSize(18);
-    text("HELLO EVERYBODY" , 80 , textY);
+    text("" , 80 , textY);
     text("" , 80 , textY + 18);
     text("" , 80 , textY + 36)
     textY -= 0.67;
