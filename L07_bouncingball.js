e63b17be6fbb805a6e66e4a211a08014c4483ba3
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-12 06:29:40 +0000

    Sep 12, 2026, 6:29 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,11 +1,30 @@
-ballX = 200;
-ballY = 200;
-col = 0,0,0,1;
+let ballX = 200;
+let ballY = 200;
+let colrgba = (0,0,0,1);
+let cmxnum = 5;
+let cmynum = 5; 
 function setup() {
     createCanvas(400,400);
     background(220);
 }
 function draw() {
     circle(ballX,ballY,50);
-    
+    if(ballX = 0) {
+        cmxnum = 5;
+        colrgba = (random(0,255),random(0,255),random(0,255),random(0.3,1.0));
+    }
+    if(ballx = 350) {
+        cmxnum = -5;
+        colrgba = (random(0,255),random(0,255),random(0,255),random(0.3,1.0));
+    }
+    if(ballY = 0) {
+        cmynum = 5;
+        colrgba = (random(0,255),random(0,255),random(0,255),random(0.3,1.0));
+    }
+    if(ballY = 350) {
+        cmynum = -5;
+        colrgba = (random(0,255),random(0,255),random(0,255),random(0.3,1.0));
+    }
+    ballX = ballX + cmxnum;
+    ballY = ballY + cmynum;
 }
