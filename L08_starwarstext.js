a349c1cb08cefdfe77ff5662dd24348429f5d103
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-19 06:23:45 +0000

    Sep 19, 2026, 6:23 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,11 +1,11 @@
-let ypos;
+let yPos;
 function setup() {
     createCanvas(600,400);
     textAlign(LEFT,CENTER);
     textSize(24);
     fill("rgb(255,255,0)");
 }
-ypos = height;
+yPos = height;
 function draw() {
     background(0);
     text("asd",width / 2, yPos);
