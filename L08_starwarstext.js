ef5fe1180a5a722a7efa743a107be5c877bd68b4
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-19 07:02:58 +0000

    Sep 19, 2026, 7:02 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -14,7 +14,7 @@ let storyText = [
 ];
 let bgmusic;
 function preload() {
-    bgmusic = loadSound(star_wars_theme_8_bit.mp3)
+    bgmusic = loadSound(star_wars_theme_8_bit.mp3);
 }
 function setup() {
     createCanvas(600,400);
