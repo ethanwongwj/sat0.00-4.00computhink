8ee747b90595a0386a34c0b26cc48e78d00cf01c
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-19 07:38:05 +0000

    Sep 19, 2026, 7:38 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -14,6 +14,6 @@ function draw() {
     second = second();
     text(hr,width /2, height/2);
     text(minuite,width/2,height/2+100);
-    text(seconde,width/2,height/2+100);;
+    text(seconde,width/2,height/2+100);
     text(timeString,width/2,height/2);
 }
