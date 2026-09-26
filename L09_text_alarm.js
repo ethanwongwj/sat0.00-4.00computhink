0105cab9f43bf7aaabdecd0991bb954a421460a0
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-26 06:09:53 +0000

    Sep 26, 2026, 6:09 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,21 +1,22 @@
-let min;
-let sec;
-let hr;
-let timeString;
-function setup() {
-    createCanvas(600,400);
-    textAlign(CENTER,CENTER);
-    textSize(24);
-    fill("rgb(20,0,255)");
-}
-function draw() {
-    background(100);
-    hr = hour();
-    min = minute();
-    sec = second();
-    // text(hr,width /2, height/2);
-    // text(min,width/2,height/2+50);
-    // text(sec,width/2,height/2+100);
-    timeString = nf(hr,2) + " : " + nf(min,2) + " : " + nf(sec,2)
-    text(timeString,width/2,height/2);
-}
+// let min;
+// let sec;
+// let hr;
+// let timeString;
+// function setup() {
+//     createCanvas(600,400);
+//     textAlign(CENTER,CENTER);
+//     textSize(24);
+//     fill("rgb(20,0,255)");
+// }
+// function draw() {
+//     background(100);
+//     hr = hour();
+//     min = minute();
+//     sec = second();
+//     // text(hr,width /2, height/2);
+//     // text(min,width/2,height/2+50);
+//     // text(sec,width/2,height/2+100);
+//     timeString = nf(hr,2) + " : " + nf(min,2) + " : " + nf(sec,2)
+//     text(timeString,width/2,height/2);
+// }
+
