9a2c785f968c008a00ae4a9d08335ffba0e01327
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-26 07:17:52 +0000

    Sep 26, 2026, 7:17 AM

--- L10_name.js
+++ L10_name.js
@@ -2,11 +2,10 @@ let inputText;
 let displayText = "Your Name Here";
 function setup() {
     createCanvas(600,400);
-    background(0)
     inputText = createInput();
 
 }
 
 function draw() {
-    
+    background(0)
 }
