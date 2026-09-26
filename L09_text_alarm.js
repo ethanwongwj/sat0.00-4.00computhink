81f9f148433ffe970c44cf31cb6a54e847ef3773
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-26 06:55:28 +0000

    Sep 26, 2026, 6:55 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -45,6 +45,6 @@ function updateCountdown() {
     }
 }
 function mousePressed() {
-    if(countdownStart = false)
+    if(countdownStart === false) {}
     intervalid = setInterval(drawCircle,1000);
 }
