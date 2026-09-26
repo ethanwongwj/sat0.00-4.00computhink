b8d24a23a4c4738966db4dc8747efd8bafd95710
Ethan Wong <wongwj.ethan@gmail.com>
2026-09-26 07:33:12 +0000

    Sep 26, 2026, 7:33 AM

--- L10_name.js
+++ L10_name.js
@@ -8,7 +8,7 @@ function setup() {
     inputText = createInput();
     inputText.input(updateText);
     inputText.position(width/2,height/2);
-    inputText(input)
+    inputText.input(updateText)
 }
 
 function draw() {
